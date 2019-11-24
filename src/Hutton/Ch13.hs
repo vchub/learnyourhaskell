af306b44f8ae9cff2021703e7b3c7c4736a81c47
vlad chub <vlad.chub@gmail.com>
2019-11-24 12:32:46 -0800

    Calculator and comment

--- src/Hutton/Ch13.hs
+++ src/Hutton/Ch13.hs
@@ -2,6 +2,9 @@
 
 module Hutton.Ch13 where
 
+import           Control.Applicative
+import           Data.Char
+import           System.IO
 
 newtype Parser a = P (String->[(a, String)])
 
@@ -42,6 +45,7 @@ three = pure g <*> item <*> item <*> item where g x _ z = (x, z)
 -- three =
 --   pure (\(x : _ : z) -> (x, z)) <*> P (\inp -> [(take 3 inp, drop 3 inp)])
 
+
 instance Monad Parser where
   -- (>>=)::Parser a -> (a->Parser b)-> Parser b
   p >>= f = P(\inp -> case parse p inp of
@@ -59,16 +63,261 @@ three1 = do
   return (x, z)
 
 
+instance Alternative Parser where
+  -- empty::f a
+  empty = P(\_ -> [])
+
+  -- (<|>)::f a->f a->f a
+  p <|> q = P(\inp -> case parse p inp of
+    []    -> parse q inp
+    (x:_) -> [x]           )
+
+sat :: (Char -> Bool) -> Parser Char
+sat g = do
+  v <- item
+  if g v then return v else empty
+
+letter :: Parser Char
+letter = sat isAlpha
+
+digit :: Parser Char
+digit = sat isDigit
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string []       = return []
+string (x : xs) = do
+  _ <- char x
+  _ <- string xs
+  return (x : xs)
+
+some1 :: Parser a -> Parser [a]
+some1 p = pure (:) <*> p <*> many1 p
+
+many1 :: Parser a -> Parser [a]
+many1 p = some1 p <|> pure []
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+ident :: Parser String
+ident = do
+  x  <- lower
+  xs <- many alphanum
+  return (x : xs)
+
+nat :: Parser Int
+nat = do
+  x <- some digit
+  return (read x)
+
+space :: Parser ()
+space = do
+  _ <- many (sat isSpace)
+  return ()
+
+int :: Parser Int
+-- int = char '-' <*> nat <*> pure negate <|> nat
+-- int = fmap negate (char '-' <*> nat) <|> nat
+int = negnat <|> nat
+ where
+  negnat = do
+    _ <- char '-'
+    n <- nat
+    return (-n)
+
+token :: Parser a -> Parser a
+token p = do
+  _ <- space
+  x <- p
+  _ <- space
+  return x
+
+identfier :: Parser String
+identfier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+integer :: Parser Int
+integer = token int
+
+symbol :: String -> Parser String
+symbol s = token (string s)
+
+nats :: Parser [Int]
+nats = do
+  _  <- symbol "["
+  x  <- natural
+  xs <- some
+    (do
+      _ <- symbol ","
+      n <- natural
+      return n
+    )
+  _ <- symbol "]"
+  return (x : xs)
+
+-- takeWhileP :: (Char -> Bool) -> Parser String
+-- takeWhileP g = do
+--   c <- item
+--   if g c
+--     then do
+--       cs <- takeWhileP g
+--       return (c : cs)
+--     else return [c]
+
+comment :: Parser ()
+comment = do
+  _ <- symbol "--"
+  _ <- many (sat (/= '\n'))
+  _ <- item
+  return ()
 
 
 
 
 
+-- ====================
+-- Arigthmetic expression Parser
+
+expr :: Parser Int
+expr = sumexp <|> term
+ where
+  sumexp =
+    (do
+      x  <- term
+      op <- symbol "+" <|> symbol "-"
+      y  <- expr
+      case op of
+        "+" -> return (x + y)
+        "-" -> return (x - y)
+        _   -> empty
+    )
+
+term :: Parser Int
+term = prodexp <|> factor
+ where
+  prodexp =
+    (do
+      x  <- factor
+      op <- symbol "*" <|> symbol "/"
+      y  <- expr
+      case op of
+        "*" -> return (x * y)
+        "/" -> return (x `div` y)
+        _   -> empty
+    )
+
+factor :: Parser Int
+factor = parentheses <|> integer
+ where
+  parentheses =
+    (do
+      _ <- symbol "("
+      e <- expr
+      _ <- symbol ")"
+      return e
+    )
+
+eval :: String -> Int
+eval xs = case parse expr xs of
+  [(n, [] )] -> n
+  [(_, out)] -> error ("Unused input " ++ out)
+  []         -> error ("Invalid input")
+  (_ : _)    -> error ("eval: Shouldn't be the case")
+
+-- ====================
+-- Calculator
+
+buttons :: String
+buttons = standard ++ extra
+ where
+  standard = "qcd=123+456-789*0()/"
+  extra    = "QCD \ESC\BS\DEL\n"
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int, Int)
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do
+  goto p
+  putStr xs
+
+goto :: Pos -> IO ()
+goto (x, y) = putStr ("\ESC[" ++ (show y) ++ ";" ++ (show x) ++ "H")
+
+getCh :: IO Char
+getCh = do
+  hSetEcho stdin False
+  x <- getChar
+  hSetEcho stdin True
+  return x
+
+calc :: String -> IO ()
+calc xs = do
+  c <- getCh
+  if elem c buttons
+    then process c xs
+    else do
+      beep
+      calc xs
+
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC"    = quit
+             | elem c "dD\BS\DEL" = delete xs
+             | elem c "=\n"       = eval1 xs
+             | elem c "cC"        = clear
+             | otherwise          = press c xs
 
 
+beep :: IO ()
+beep = putStr "\BEL"
 
+quit :: IO ()
+quit = putStrLn "Buy"
 
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = do
+  cls
+  writeat (1, 1) (init xs)
+  calc (init xs)
 
+eval1 :: String -> IO ()
+eval1 xs = case parse expr xs of
+  [(n, [])] -> do
+    cls
+    calc (show n)
+  _ -> do
+    beep
+    calc xs
+
+press :: Char -> String -> IO ()
+press c xs = do
+  cls
+  writeat (1, 1) (xs ++ [c])
+  calc (xs ++ [c])
+
+clear :: IO ()
+clear = do
+  cls
+  calc []
+
+run :: IO ()
+run = do
+  cls
+  calc ""
 
 
 
--- test/Hutton/Ch13Spec.hs
+++ test/Hutton/Ch13Spec.hs
@@ -2,6 +2,8 @@
 
 module Hutton.Ch13Spec where
 
+import           Control.Applicative
+import           Control.Exception
 import           Data.Char
 import           Hutton.Ch13
 import           Test.Hspec
@@ -10,6 +12,10 @@ import           Test.Hspec
 
 spec :: Spec
 spec = describe "Ch13" $ do
+  describe "Exercises" $ do
+    -- it "comment" $ parse (takeWhileP (/= 'a')) "bdabc" `shouldBe` [("bd","abc")]
+    it "comment" $ parse comment " -- foo \n abc" `shouldBe` [(()," abc")]
+
   describe "Parser" $ do
     let s = "abc"
      in do
@@ -23,3 +29,65 @@ spec = describe "Ch13" $ do
         it "three1" $ parse three1 s`shouldBe` [(('a','c'), "")]
         it "three1 []" $ parse three1 "ab" `shouldBe` []
         it "three1" $ parse three1 "abcde"`shouldBe` [(('a','c'), "de")]
+
+        it "empty" $ parse empty s `shouldBe` ([]::[(Char, String)])
+        it "<|>" $ parse (item <|> empty) s `shouldBe` [('a', "bc")]
+        it "<|>" $ parse (empty <|> item) s `shouldBe` [('a', "bc")]
+        it "<|>" $ parse (empty <|> return 'd') "abcde"`shouldBe` [('d', "abcde")]
+
+        it "sat" $ parse (sat (<='b')) s `shouldBe` [('a', "bc")]
+        it "sat" $ parse (sat (>'c')) s `shouldBe` []
+
+        it "digit" $ parse digit "1a" `shouldBe` [('1', "a")]
+        it "digit" $ parse digit "a1" `shouldBe` []
+        it "letter" $ parse letter "a1" `shouldBe` [('a', "1")]
+        it "char" $ parse (char 'a') "a1" `shouldBe` [('a', "1")]
+        it "char" $ parse (char 'b') "a1" `shouldBe` []
+        it "string" $ parse (string "abc") "abcde" `shouldBe` [("abc", "de")]
+        it "string" $ parse (string "abc") "ab123" `shouldBe` []
+
+        it "many" $ parse (many digit) "ab123" `shouldBe` [("","ab123")]
+        it "many" $ parse (many digit) "123a" `shouldBe` [("123","a")]
+        it "many1" $ parse (many1 digit) "ab123" `shouldBe` [("","ab123")]
+        it "many1" $ parse (many1 digit) "123a" `shouldBe` [("123","a")]
+
+        it "some" $ parse (some digit) "ab123" `shouldBe` []
+        it "some" $ parse (some digit) "123a" `shouldBe` [("123","a")]
+        it "some1" $ parse (some1 digit) "ab123" `shouldBe` []
+        it "some1" $ parse (some1 digit) "123a" `shouldBe` [("123","a")]
+
+        it "ident" $ parse ident "123a" `shouldBe` []
+        it "ident" $ parse ident "b123a c" `shouldBe` [("b123a"," c")]
+
+        it "nat" $ parse nat "123 ac" `shouldBe` [(123, " ac")]
+        it "nat" $ parse nat "123ac" `shouldBe` [(123,"ac")]
+        it "nat" $ parse nat "x123ac" `shouldBe` []
+
+        it "nat" $ parse space " ac" `shouldBe` [((), "ac")]
+        it "nat" $ parse space "     ac" `shouldBe` [((), "ac")]
+
+        it "int" $ parse int "-123 ab" `shouldBe` [((-123), " ab")]
+        it "int" $ parse int "123 ab" `shouldBe` [(123, " ab")]
+
+        it "identfier" $ parse identfier "  123a " `shouldBe` []
+        it "identfier" $ parse identfier "  x123a c" `shouldBe` [("x123a", "c")]
+
+        it "nats" $ parse nats " [1 , 23,4] ab" `shouldBe` [([1,23,4], "ab")]
+        it "nats" $ parse nats " [1 , 23,4,] ab" `shouldBe` []
+
+        it "epxr" $ parse expr "1 + 3 ab" `shouldBe` [(4, "ab")]
+        it "epxr" $ parse expr "1 - 3 ab" `shouldBe` [((-2), "ab")]
+        it "epxr" $ parse expr "1 / 3 ab" `shouldBe` [(0, "ab")]
+        it "epxr" $ parse expr "5 / 3 ab" `shouldBe` [(1, "ab")]
+
+        it "epxr" $ parse expr "1+3 ab" `shouldBe` [(4, "ab")]
+        it "epxr" $ parse expr "4*(1+3) ab" `shouldBe` [(16, "ab")]
+        it "epxr" $ parse expr "(2+1)*(1+3) ab" `shouldBe` [(12, "ab")]
+
+        it "eval" $ eval "(2+1)*(1+3) " `shouldBe` 12
+        it "eval" $ evaluate (eval "(2+1)*(1+3) ab") `shouldThrow` anyException
+        it "eval" $ eval "(-2+8)/(5-3) " `shouldBe` 3
+
+
+
+
