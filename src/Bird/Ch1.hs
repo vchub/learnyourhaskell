1b93031cfb93f2cc4c002771a950836599acbade
vlad chub <vlad.chub@gmail.com>
2019-12-03 23:51:40 -0800

    num to words

--- src/Bird/Ch1.hs
+++ src/Bird/Ch1.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module Bird.Ch1 where
 
+import           Control.Applicative
 import           Data.List
 import qualified Data.Map                      as M
 import qualified Data.Text                     as T
@@ -30,6 +31,46 @@ countRuns =
   go []       = []
   go (x : xs) = (x, length l) : go r where (l, r) = break (x <) (x : xs)
 
+numName :: M.Map Int String
+numName = M.fromList
+  [ (1 , "one")
+  , (2 , "two")
+  , (3 , "three")
+  , (11, "eleven")
+  , (12, "twelve")
+  , (20, "twenty ")
+  ]
+
+fromPQ :: Int -> Int -> Maybe String
+fromPQ n 100   = M.lookup n numName
+fromPQ n 1000  = fmap (++ " hundred ") (M.lookup n numName)
+fromPQ n 10000 = fmap (++ " thousand ") (M.lookup n numName)
+fromPQ _ _     = error "fromPQ not implemented"
+-- fromPQ p 20   = M.lookup p numName
+-- fromPQ p 10  = M.lookup (10 + p) numName
+
+sayNumber :: Int -> Maybe String
+sayNumber x = go x 100 (Just "")
+ where
+  go :: Int -> Int -> Maybe String -> Maybe String
+  go 0 _ acc = acc
+  go n i acc
+    | n < 20 = pure (++) <*> (fromPQ n i) <*> acc
+    | n < 100 = foldl (liftA2 (++))
+                      acc
+                      [(fromPQ ((div n 10) * 10) 100), (fromPQ (mod n 10) i)]
+    |
+    -- TODO: that's wrong
+      otherwise = go (div n 100)
+                     (i * 10)
+                     (pure (++) <*> (fromPQ (mod n i) i) <*> acc)
+
+
+-- (pure (++) <*> (Just "some") <*> Just " foo")
+-- a = Just 2
+-- b = Just 2
+-- pure (*) <*> a <*> b <*> a
+-- foldl (liftA2 (+)) a [a,b,a]
 
 {--
 break (1<) [1,3,4]
--- test/Bird/Ch1Spec.hs
+++ test/Bird/Ch1Spec.hs
@@ -22,3 +22,24 @@ spec = describe "Ch1" $ do
       -- it "squareCount" $ M.toList (squareCount s) `shouldBe` [("a", 2)]
       it "squareCount" $ squareCount s `shouldBe` [("a", 2),("t", 2),("b", 1)]
 
+  describe "sayNumber" $ do
+    it "fromPQ" $ fromPQ 1 100  `shouldBe` Just "one"
+    it "eleven" $ fromPQ 11 100  `shouldBe` Just "eleven"
+    it "fromPQ" $ fromPQ 3 1000  `shouldBe` Just "three hundred "
+    it "sayNumber" $ sayNumber 1  `shouldBe` Just "one"
+    it "sayNumber 11" $ sayNumber 11  `shouldBe` Just "eleven"
+    it "sayNumber 21" $ sayNumber 21  `shouldBe` Just "twenty one"
+    it "sayNumber 101" $ sayNumber 101  `shouldBe` Just "one hundred one"
+
+
+
+
+
+
+
+
+
+
+
+
+
